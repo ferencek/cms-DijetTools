28a93bc2e2069a5ce49ebb967ed58c0e13cdff8d
ferencek <>
2012-02-04 21:11:18 +0000

    added function for plotting efficiencies for inidividual samples before merging

--- bTaggingEfficiency.C
+++ bTaggingEfficiency.C
@@ -278,6 +278,94 @@ void bTaggingEfficiencyVsPtJ2_ext(const string& fInputFile1, const string& fInpu
 }
 
 
+void bTaggingEfficiencyVsPt(const string& fInputFile1, const string& fInputFile2, const string& fPlot1, const string& fPlot2, const string& fAlgo,
+                                   const string& fTitle, const string& fLabel, const string& fLegend1, const string& fLegend2, const string& fOutputFile,
+                                   const Int_t nbins, const Double_t *xbins, const string& fXAxisTitle, const string& fYAxisTitle)
+{
+  gROOT->SetBatch(kTRUE);
+
+  TFile *file1 = new TFile(fInputFile1.c_str());
+  TFile *file2 = new TFile(fInputFile2.c_str());
+
+  TH2D *h2_denom_Sample1 = (TH2D*)file1->Get((fPlot1 + "_HF").c_str());
+  TH2D *h2_num_Sample1 = (TH2D*)file1->Get((fPlot1 + "_" + fAlgo).c_str());
+
+  TH1D *h1_denom_Sample1 = h2_denom_Sample1->ProjectionX("_px",48,52);
+  TH1D *h1_num_Sample1 = h2_num_Sample1->ProjectionX("_px",48,52);
+
+  TH1D *h1_denom_Sample1_rebinned = (TH1D*)h1_denom_Sample1->Rebin(nbins,"h1_denom_Sample1_rebinned",xbins);
+  TH1D *h1_num_Sample1_rebinned = (TH1D*)h1_num_Sample1->Rebin(nbins,"h1_num_Sample1_rebinned",xbins);
+
+  TH2D *h2_denom_Sample2 = (TH2D*)file2->Get((fPlot2 + "_HF").c_str());
+  TH2D *h2_num_Sample2 = (TH2D*)file2->Get((fPlot2 + "_" + fAlgo).c_str());
+
+  TH1D *h1_denom_Sample2 = h2_denom_Sample2->ProjectionX("_px",48,52);
+  TH1D *h1_num_Sample2 = h2_num_Sample2->ProjectionX("_px",48,52);
+
+  TH1D *h1_denom_Sample2_rebinned = (TH1D*)h1_denom_Sample2->Rebin(nbins,"h1_denom_Sample2_rebinned",xbins);
+  TH1D *h1_num_Sample2_rebinned = (TH1D*)h1_num_Sample2->Rebin(nbins,"h1_num_Sample2_rebinned",xbins);
+
+  TCanvas *c = new TCanvas("c", "",800,800);
+  c->cd();
+
+  TGraphAsymmErrors *g_efficiency_Sample1 = new TGraphAsymmErrors(h1_num_Sample1_rebinned, h1_denom_Sample1_rebinned,"cp");
+//   g_efficiency_Sample1->SetMarkerSize(0.8);
+  g_efficiency_Sample1->SetMarkerStyle(20);
+  g_efficiency_Sample1->SetMarkerColor(kBlack);
+  g_efficiency_Sample1->SetTitle(fTitle.c_str());
+  g_efficiency_Sample1->GetXaxis()->SetTitle(fXAxisTitle.c_str());
+  g_efficiency_Sample1->GetYaxis()->SetTitle(fYAxisTitle.c_str());
+//   g_efficiency_Sample1->GetXaxis()->SetRangeUser(xMin,xMax);
+  g_efficiency_Sample1->GetYaxis()->SetRangeUser(0,1.);
+
+  TGraphAsymmErrors *g_efficiency_Sample2 = new TGraphAsymmErrors(h1_num_Sample2_rebinned, h1_denom_Sample2_rebinned,"cp");
+//   g_efficiency_Sample2->SetMarkerSize(0.8);
+  g_efficiency_Sample2->SetMarkerStyle(24);
+  g_efficiency_Sample2->SetMarkerColor(kRed);
+  g_efficiency_Sample2->SetLineColor(kRed);
+  g_efficiency_Sample2->SetTitle(fTitle.c_str());
+  g_efficiency_Sample2->GetXaxis()->SetTitle(fXAxisTitle.c_str());
+  g_efficiency_Sample2->GetYaxis()->SetTitle(fYAxisTitle.c_str());
+//   g_efficiency_Sample2->GetXaxis()->SetRangeUser(xMin,xMax);
+
+  g_efficiency_Sample1->Draw("AP");
+  g_efficiency_Sample2->Draw("Psame");
+
+  TLegend *legend = new TLegend(.55,.7,.85,.85);
+  legend->SetBorderSize(0);
+  legend->SetFillColor(0);
+  legend->SetFillStyle(0);
+  legend->AddEntry(g_efficiency_Sample1,fLegend1.c_str(),"lp");
+  legend->AddEntry(g_efficiency_Sample2,fLegend2.c_str(),"lp");
+  legend->Draw();
+
+//   TLine *line = new TLine(xMin,0.99,xMax,0.99);
+//   line->SetLineWidth(3.);
+//   line->SetLineColor(kGreen+2);
+//   line->SetLineStyle(7);
+//   line->Draw("same");
+
+  TLatex l1;
+  l1.SetTextAlign(12);
+//   l1.SetTextFont(42);
+  l1.SetNDC();
+  l1.SetTextSize(0.04);
+//   l1.DrawLatex(0.5,0.35,"#sqrt{s} = 7 TeV");
+//   l1.DrawLatex(0.5,0.30,"Anti-k_{T} R = 0.7 PFJets");
+  l1.DrawLatex(0.2,0.8,fLabel.c_str());
+
+//   c->SetLogy();
+  c->SaveAs(fOutputFile.c_str());
+
+//   delete line;
+//   delete h_Npass;
+//   delete h_Ntotal;
+//   delete g_efficiency_Sample1;
+  delete c;
+//   delete file;
+}
+
+
 void makePlots()
 {
 
@@ -303,6 +391,7 @@ void makePlots()
 //                            "TCHEM, b parton matching", "|#eta|<0.2",
 //                            "b-tag_eff_PtJ1_etalt0p2_QCD_Pythia6_MadGraph_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
 
+  
 //   bTaggingEfficiencyVsPtJ1_ext("CRAB_Jobs_bTaggingEfficiency_bPartonMatching/Final__histograms.root",
 //                                "CRAB_Jobs_bTaggingEfficiency_QCD_FCR_bPartonMatching/Final__histograms.root",
 //                                "QCD_Pythia6", "QCD_Pythia6" , "TCHEM",
@@ -315,16 +404,82 @@ void makePlots()
 //                                "TCHEM, b parton matching", "|#eta|<0.2", "Z'#rightarrowb#bar{b}", "RS Graviton",
 //                                "b-tag_eff_PtJ1_etalt0p2_ZprimeBBbar_RSGraviton_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
 
-  bTaggingEfficiencyVsPtJ1_ext("CRAB_Jobs_bTaggingEfficiency_bPartonMatching/Final__histograms.root",
-                               "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
-                               "QCD_Pythia6", "QCD_Pythia6" , "TCHEM",
-                               "TCHEM, b parton matching", "|#eta|<0.2", "QCD Pythia6", "QCD Pythia6 GSP",
-                               "b-tag_eff_PtJ1_etalt0p2_QCD_GSP_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
+  
+//   bTaggingEfficiencyVsPtJ1_ext("CRAB_Jobs_bTaggingEfficiency_bPartonMatching/Final__histograms.root",
+//                                "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
+//                                "QCD_Pythia6", "QCD_Pythia6" , "TCHEM",
+//                                "TCHEM, b parton matching", "|#eta|<0.2", "QCD Pythia6", "QCD Pythia6 GSP",
+//                                "b-tag_eff_PtJ1_etalt0p2_QCD_GSP_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
 
-  bTaggingEfficiencyVsPtJ2_ext("CRAB_Jobs_bTaggingEfficiency_bPartonMatching/Final__histograms.root",
-                               "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
-                               "QCD_Pythia6", "QCD_Pythia6" , "TCHEM",
-                               "TCHEM, b parton matching", "|#eta|<0.2", "QCD Pythia6", "QCD Pythia6 GSP",
-                               "b-tag_eff_PtJ2_etalt0p2_QCD_GSP_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,2} [GeV]", "b-tag efficiency");
+//   bTaggingEfficiencyVsPtJ2_ext("CRAB_Jobs_bTaggingEfficiency_bPartonMatching/Final__histograms.root",
+//                                "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
+//                                "QCD_Pythia6", "QCD_Pythia6" , "TCHEM",
+//                                "TCHEM, b parton matching", "|#eta|<0.2", "QCD Pythia6", "QCD Pythia6 GSP",
+//                                "b-tag_eff_PtJ2_etalt0p2_QCD_GSP_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,2} [GeV]", "b-tag efficiency");
+
+  
+  bTaggingEfficiencyVsPt("CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
+                         "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/QCD_Pt-170to300_TuneZ2_7TeV_pythia6__ferencek-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04_skim-v2__histograms.root",
+                         "QCD_Pythia6__h2_EtaJ1_vs_PtJ1",
+                         "myAnalyzer/h2_EtaJ1_vs_PtJ1" ,
+                         "TCHEM", "TCHEM, b parton matching", "|#eta|<0.2",
+                         "QCD GSP", "QCD GSP Pt170to300",
+                         "b-tag_eff_PtJ1_etalt0p2_QCD_GSP_Incl_Pt170to300_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
+
+  bTaggingEfficiencyVsPt("CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
+                         "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/QCD_Pt-300to470_TuneZ2_7TeV_pythia6__ferencek-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04_skim-v2__histograms.root",
+                         "QCD_Pythia6__h2_EtaJ1_vs_PtJ1",
+                         "myAnalyzer/h2_EtaJ1_vs_PtJ1" ,
+                         "TCHEM", "TCHEM, b parton matching", "|#eta|<0.2",
+                         "QCD GSP", "QCD GSP Pt300to470",
+                         "b-tag_eff_PtJ1_etalt0p2_QCD_GSP_Incl_Pt300to470_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
+
+  bTaggingEfficiencyVsPt("CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
+                         "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/QCD_Pt-470to600_TuneZ2_7TeV_pythia6__elhughes-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root",
+                         "QCD_Pythia6__h2_EtaJ1_vs_PtJ1",
+                         "myAnalyzer/h2_EtaJ1_vs_PtJ1" ,
+                         "TCHEM", "TCHEM, b parton matching", "|#eta|<0.2",
+                         "QCD GSP", "QCD GSP Pt470to600",
+                         "b-tag_eff_PtJ1_etalt0p2_QCD_GSP_Incl_Pt470to600_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
+  
+  bTaggingEfficiencyVsPt("CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
+                         "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/QCD_Pt-600to800_TuneZ2_7TeV_pythia6__elhughes-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root",
+                         "QCD_Pythia6__h2_EtaJ1_vs_PtJ1",
+                         "myAnalyzer/h2_EtaJ1_vs_PtJ1" ,
+                         "TCHEM", "TCHEM, b parton matching", "|#eta|<0.2",
+                         "QCD GSP", "QCD GSP Pt600to800",
+                         "b-tag_eff_PtJ1_etalt0p2_QCD_GSP_Incl_Pt600to800_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
+  
+  bTaggingEfficiencyVsPt("CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
+                         "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/QCD_Pt-800to1000_TuneZ2_7TeV_pythia6__elhughes-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root",
+                         "QCD_Pythia6__h2_EtaJ1_vs_PtJ1",
+                         "myAnalyzer/h2_EtaJ1_vs_PtJ1" ,
+                         "TCHEM", "TCHEM, b parton matching", "|#eta|<0.2",
+                         "QCD GSP", "QCD GSP Pt800to1000",
+                         "b-tag_eff_PtJ1_etalt0p2_QCD_GSP_Incl_Pt800to1000_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
+
+  bTaggingEfficiencyVsPt("CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
+                         "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/QCD_Pt-1000to1400_TuneZ2_7TeV_pythia6__elhughes-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root",
+                         "QCD_Pythia6__h2_EtaJ1_vs_PtJ1",
+                         "myAnalyzer/h2_EtaJ1_vs_PtJ1" ,
+                         "TCHEM", "TCHEM, b parton matching", "|#eta|<0.2",
+                         "QCD GSP", "QCD GSP Pt1000to1400",
+                         "b-tag_eff_PtJ1_etalt0p2_QCD_GSP_Incl_Pt1000to1400_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
+  
+  bTaggingEfficiencyVsPt("CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
+                         "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/QCD_Pt-1400to1800_TuneZ2_7TeV_pythia6__elhughes-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root",
+                         "QCD_Pythia6__h2_EtaJ1_vs_PtJ1",
+                         "myAnalyzer/h2_EtaJ1_vs_PtJ1" ,
+                         "TCHEM", "TCHEM, b parton matching", "|#eta|<0.2",
+                         "QCD GSP", "QCD GSP Pt1400to1800",
+                         "b-tag_eff_PtJ1_etalt0p2_QCD_GSP_Incl_Pt1400to1800_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
+
+  bTaggingEfficiencyVsPt("CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/Final__histograms.root",
+                         "CRAB_Jobs_bTaggingEfficiency_QCD_GSP_bPartonMatching/QCD_Pt-1800_TuneZ2_7TeV_pythia6__elhughes-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root",
+                         "QCD_Pythia6__h2_EtaJ1_vs_PtJ1",
+                         "myAnalyzer/h2_EtaJ1_vs_PtJ1" ,
+                         "TCHEM", "TCHEM, b parton matching", "|#eta|<0.2",
+                         "QCD GSP", "QCD GSP Pt1800",
+                         "b-tag_eff_PtJ1_etalt0p2_QCD_GSP_Incl_Pt1800_Pythia6_bPartonMatching.png", 15, PtBins, "p_{T,1} [GeV]", "b-tag efficiency");
   
 }
