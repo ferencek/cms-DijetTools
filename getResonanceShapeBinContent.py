39300a43a551d558469d44be4d2ee26b85722d2d
ferencek <>
2012-03-27 21:34:30 +0000

    synchronized with the updated RSG analysis code

--- getResonanceShapeBinContent.py
+++ getResonanceShapeBinContent.py
@@ -3,15 +3,22 @@
 import os
 from ROOT import *
 
-path_prefix = 'RSG_resonance_shape_files'
-histo_name = 'myAnalyzer/cutHisto_allPreviousCuts________x_bbbar'
+path_prefix = 'Resonance_shape_files/CRAB_Jobs_RSGravitonToBBbar_ResonanceShapes'
+
+histo_names = [
+    'myAnalyzer/cutHisto_allPreviousCuts________x_dist_500',
+    'myAnalyzer/cutHisto_allPreviousCuts________x_dist_700',
+    'myAnalyzer/cutHisto_allPreviousCuts________x_dist_1200',
+    'myAnalyzer/cutHisto_allPreviousCuts________x_dist_2000',
+    'myAnalyzer/cutHisto_allPreviousCuts________x_dist_3500'
+]
 
 files = [
-    'RSG_M_500__histograms.root',
-    'RSG_M_700__histograms.root',
-    'RSG_M_1200__histograms.root',
-    'RSG_M_2000__histograms.root',
-    'RSG_M_3500__histograms.root'
+    'RSGravitonToJJ_M-500_TuneZ2_7TeV_pythia6__ferencek-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root',
+    'RSGravitonToJJ_M-700_TuneZ2_7TeV_pythia6__ferencek-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root',
+    'RSGravitonToJJ_M-1200_TuneZ2_7TeV_pythia6__ferencek-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root',
+    'RSGravitonToJJ_M-2000_TuneZ2_7TeV_pythia6__ferencek-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root',
+    'RSGravitonToJJ_M-3500_TuneZ2_7TeV_pythia6__ferencek-Summer11-PU_S4_START42_V11-v1_EDMTuple_V00-00-04__histograms.root'
 ]
 
 array_names = [
@@ -24,7 +31,7 @@ array_names = [
 
 for i, fl in enumerate(files):
   file = TFile(os.path.join(path_prefix,fl))
-  histo = file.Get(histo_name)
+  histo = file.Get(histo_names[i])
 
   array_string = array_names[i] + ' = {'
   
