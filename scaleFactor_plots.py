d7386aba15351e070cb92d148bd4459c967bb842
ferencek <>
2012-06-15 23:27:37 +0000

    added CSVM SFs

--- scaleFactor_plots.py
+++ scaleFactor_plots.py
@@ -65,6 +65,42 @@ CSVL_SFl_0p5to1p0_max = TF1("CSVL_SFl_0p5to1p0_max","((1.15882+(0.000579711*x))+
 CSVL_SFl_1p0to1p5_max = TF1("CSVL_SFl_1p0to1p5_max","((1.16292+(0.000659848*x))+(-2.07868e-06*(x*x)))+(1.72763e-09*(x*(x*x)))", 20.,670.)
 CSVL_SFl_1p5to2p4_max = TF1("CSVL_SFl_1p5to2p4_max","((1.14357+(0.00020854*x))+(-7.43519e-07*(x*x)))+(8.73742e-10*(x*(x*x)))", 20.,670.)
 
+# CSVM
+CSVM_SFb_0to2p4 = TF1("CSVM_SFb_0to2p4","0.6981*((1.+(0.414063*x))/(1.+(0.300155*x)))", 30.,670.)
+
+CSVM_SFb_errors = TH1D("CSVM_SFb_errors", "CSVM_SFb_errors", 14, PtBins_b)
+CSVM_SFb_errors.SetBinContent( 0,0.12)
+CSVM_SFb_errors.SetBinContent( 1,0.0295675)
+CSVM_SFb_errors.SetBinContent( 2,0.0295095)
+CSVM_SFb_errors.SetBinContent( 3,0.0210867)
+CSVM_SFb_errors.SetBinContent( 4,0.0219349)
+CSVM_SFb_errors.SetBinContent( 5,0.0227033)
+CSVM_SFb_errors.SetBinContent( 6,0.0204062)
+CSVM_SFb_errors.SetBinContent( 7,0.0185857)
+CSVM_SFb_errors.SetBinContent( 8,0.0256242)
+CSVM_SFb_errors.SetBinContent( 9,0.0383341)
+CSVM_SFb_errors.SetBinContent(10,0.0409675)
+CSVM_SFb_errors.SetBinContent(11,0.0420284)
+CSVM_SFb_errors.SetBinContent(12,0.0541299)
+CSVM_SFb_errors.SetBinContent(13,0.0578761)
+CSVM_SFb_errors.SetBinContent(14,0.0655432)
+CSVM_SFb_errors.SetBinContent(15,(2*0.0655432))
+
+CSVM_SFl_0to2p4 =   TF1("CSVM_SFl_0to2p4","((1.04318+(0.000848162*x))+(-2.5795e-06*(x*x)))+(1.64156e-09*(x*(x*x)))", 20.,670.)
+CSVM_SFl_0to0p8 =   TF1("CSVM_SFl_0to0p8","((1.06182+(0.000617034*x))+(-1.5732e-06*(x*x)))+(3.02909e-10*(x*(x*x)))", 20.,670.)
+CSVM_SFl_0p8to1p6 = TF1("CSVM_SFl_0p8to1p6","((1.111+(-9.64191e-06*x))+(1.80811e-07*(x*x)))+(-5.44868e-10*(x*(x*x)))", 20.,670.)
+CSVM_SFl_1p6to2p4 = TF1("CSVM_SFl_1p6to2p4","((1.08498+(-0.000701422*x))+(3.43612e-06*(x*x)))+(-4.11794e-09*(x*(x*x)))", 20.,670.)
+
+CSVM_SFl_0to2p4_min =   TF1("CSVM_SFl_0to2p4_min","((0.962627+(0.000448344*x))+(-1.25579e-06*(x*x)))+(4.82283e-10*(x*(x*x)))", 20.,670.)
+CSVM_SFl_0to0p8_min =   TF1("CSVM_SFl_0to0p8_min","((0.972455+(7.51396e-06*x))+(4.91857e-07*(x*x)))+(-1.47661e-09*(x*(x*x)))", 20.,670.)
+CSVM_SFl_0p8to1p6_min = TF1("CSVM_SFl_0p8to1p6_min","((1.02055+(-0.000378856*x))+(1.49029e-06*(x*x)))+(-1.74966e-09*(x*(x*x)))", 20.,670.)
+CSVM_SFl_1p6to2p4_min = TF1("CSVM_SFl_1p6to2p4_min","((0.983476+(-0.000607242*x))+(3.17997e-06*(x*x)))+(-4.01242e-09*(x*(x*x)))", 20.,670.)
+
+CSVM_SFl_0to2p4_max =   TF1("CSVM_SFl_0to2p4_max","((1.12368+(0.00124806*x))+(-3.9032e-06*(x*x)))+(2.80083e-09*(x*(x*x)))", 20.,670.)
+CSVM_SFl_0to0p8_max =   TF1("CSVM_SFl_0to0p8_max","((1.15116+(0.00122657*x))+(-3.63826e-06*(x*x)))+(2.08242e-09*(x*(x*x)))", 20.,670.)
+CSVM_SFl_0p8to1p6_max = TF1("CSVM_SFl_0p8to1p6_max","((1.20146+(0.000359543*x))+(-1.12866e-06*(x*x)))+(6.59918e-10*(x*(x*x)))", 20.,670.)
+CSVM_SFl_1p6to2p4_max = TF1("CSVM_SFl_1p6to2p4_max","((1.18654+(-0.000795808*x))+(3.69226e-06*(x*x)))+(-4.22347e-09*(x*(x*x)))", 20.,670.)
+
 
 def plot_SFl(mass_min, mass_max, n_steps, SFl_mean, SFl_min, SFl_max, xAxisTitle, yAxisTitle, label, outputFilename):
 
@@ -202,7 +238,7 @@ def plot_SFb(mass_min, mass_max, n_steps, SFb_mean, SFb_error, xAxisTitle, yAxis
   
   
 if __name__ == "__main__":
-
+  # CSVL
   plot_SFl(30., 669.99, 100, CSVL_SFl_0to2p4, CSVL_SFl_0to2p4_min, CSVL_SFl_0to2p4_max, "p_{T} [GeV]", "SF_{l}", "#splitline{CSVL}{|#eta| < 2.4}", "SFl_eta0to2p4.eps")
   plot_SFl(30., 669.99, 100, CSVL_SFl_0to0p5, CSVL_SFl_0to0p5_min, CSVL_SFl_0to0p5_max, "p_{T} [GeV]", "SF_{l}", "#splitline{CSVL}{|#eta| < 0.5}", "SFl_eta0to0p5.eps")
   plot_SFl(30., 669.99, 100, CSVL_SFl_0p5to1p0, CSVL_SFl_0p5to1p0_min, CSVL_SFl_0p5to1p0_max, "p_{T} [GeV]", "SF_{l}", "#splitline{CSVL}{0.5 #leq |#eta| < 1.0}", "SFl_eta0p5to1p0.eps")
@@ -210,3 +246,12 @@ if __name__ == "__main__":
   plot_SFl(30., 669.99, 100, CSVL_SFl_1p5to2p4, CSVL_SFl_1p5to2p4_min, CSVL_SFl_1p5to2p4_max, "p_{T} [GeV]", "SF_{l}", "#splitline{CSVL}{1.5 #leq |#eta| < 2.4}", "SFl_eta1p5to2p4.eps")
 
   plot_SFb(30., 669.99, 100, CSVL_SFb_0to2p4, CSVL_SFb_errors, "p_{T} [GeV]", "SF_{b}", "#splitline{CSVL}{|#eta| < 2.4}", "SFb_eta0to2p4.eps")
+
+  # CSVM
+  plot_SFl(30., 669.99, 100, CSVM_SFl_0to2p4, CSVM_SFl_0to2p4_min, CSVM_SFl_0to2p4_max, "p_{T} [GeV]", "SF_{l}", "#splitline{CSVM}{|#eta| < 2.4}", "SFl_CSVM_eta0to2p4.eps")
+  plot_SFl(30., 669.99, 100, CSVM_SFl_0to0p8, CSVM_SFl_0to0p8_min, CSVM_SFl_0to0p8_max, "p_{T} [GeV]", "SF_{l}", "#splitline{CSVM}{|#eta| < 0.8}", "SFl_CSVM_eta0to0p8.eps")
+  plot_SFl(30., 669.99, 100, CSVM_SFl_0p8to1p6, CSVM_SFl_0p8to1p6_min, CSVM_SFl_0p8to1p6_max, "p_{T} [GeV]", "SF_{l}", "#splitline{CSVM}{0.8 #leq |#eta| < 1.6}", "SFl_CSVM_eta0p8to1p6.eps")
+  plot_SFl(30., 669.99, 100, CSVM_SFl_1p6to2p4, CSVM_SFl_1p6to2p4_min, CSVM_SFl_1p6to2p4_max, "p_{T} [GeV]", "SF_{l}", "#splitline{CSVM}{1.6 #leq |#eta| < 2.4}", "SFl_CSVM_eta1p6to2p4.eps")
+
+  plot_SFb(30., 669.99, 100, CSVM_SFb_0to2p4, CSVM_SFb_errors, "p_{T} [GeV]", "SF_{b}", "#splitline{CSVM}{|#eta| < 2.4}", "SFb_CSVM_eta0to2p4.eps")
+  
